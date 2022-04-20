5d618691978118ffb2afe9077e41facb9d2fb9ae
NikanV <nikanvsiuni@gmail.com>
2022-04-20 12:48:01 +0430

    added the related enums for the units

--- src/main/java/enums/units/Ranged.java
+++ src/main/java/enums/units/Ranged.java
@@ -1,4 +1,20 @@
 package enums.units;
 
 public enum Ranged {
+    ARCHER("//s*archer//s*"),
+    CHARIOT_ARCHER("//s*chariot_archer//s*"),
+    CATAPULT("//s*catapult//s*"),
+    CROSSBOW_MAN("//s*crossbow_man//s*"),
+    TREBUCHET("//s*trebuchet//s*"),
+    CANON("//s*canon//s*"),
+    ARTILLERY("//s*artillery//s*"),
+    PANZER("//s*panzer//s*"),
+    TANK("//s*tank//s*");
+
+
+    private final String regex;
+
+    Ranged(String regex) {
+        this.regex = regex;
+    }
 }
