cb789958e340d0d39af004d8c432e1e2c1a66829
Ashakan Majidi <ashkan.majidi.1382@gmail.com>
2022-04-24 16:17:27 +0430

    showMapfunction

--- src/main/java/models/World.java
+++ src/main/java/models/World.java
@@ -11,19 +11,23 @@ public class World {
 
     private int year;
 
-    private Tile[][] map = null;
-
+    public final int width = 5;
+    public final int length = 12;
+    private Tile[][] map = new Tile[width][length];
     private int turn;
 
     public World(ArrayList<String> players) {
-
         generateMap();
     }
 
+    public Tile[][] getMap() {
+        return this.map;
+    }
+
     public void generateMap() {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 12; j++) {
-                map[i][j] = Tile.generateRandomTile();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < length; j++) {
+                this.map[i][j] = Tile.generateRandomTile();
             }
         }
     }
--- src/main/java/views/GamePlay.java
+++ src/main/java/views/GamePlay.java
@@ -17,8 +17,9 @@ public class GamePlay {
     public void run(ArrayList<String> usernames) {
         // TODO user validation
         Tile.readTileTypesInformationFromJson();
-
         world = new World(usernames);
+        this.showMap();
+
     }
 
     public void showResearches(Civilization civilization) {
@@ -105,6 +106,132 @@ public class GamePlay {
 
     }
 
+    public void showMap() {
+        Tile[][] map = world.getMap();
+        int m = world.width;
+        int n = world.length;
+        int x = -1;
+        int y = -1;
+        boolean E = true;
+        boolean O = false;
+        boolean printingCordinatesFlag = false;
+        String cordinates = "";
+        int currentChar = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 3; j >= 1; j--) {
+                x = -1;
+                y = -1;
+                for (int k = 1; k <= 8 * n + 3; k++) {
+                    if (j == 1 && k % 16 == 4) {
+                        printingCordinatesFlag = true;
+                        cordinates = x + "," + y;
+                    }
+                    if ((k - j) % 16 == 0 && (i > 1 || k < 8 * n)) {// on khar kaj akhary chap nashe
+                        System.out.print("/");
+                        y++;
+                        x++;
+                    } else if ((k - (12 - j)) % 16 == 0) {
+                        System.out.print("\\");
+                        y--;
+                        x++;
+                    } else if (j == 1 && (k % 16 == 12 || k % 16 == 13 || k % 16 == 14 || k % 16 == 15 || k % 16 == 0))
+                        System.out.print("_");
+                    else {
+                        if (printingCordinatesFlag) {
+                            System.out.print(cordinates.charAt(currentChar));
+                            currentChar++;
+                            if (currentChar > (cordinates.length() - 1)) {
+                                currentChar = 0;
+                                printingCordinatesFlag = false;
+                            }
+                        } else
+                            System.out.print(" ");
+                    }
+
+                }
+                System.out.println();
+            }
+            y = m - 1;
+            for (int j = 1; j <= 3; j++) {
+                for (int k = 1; k <= 8 * n + 3; k++) {
+                    x = -1;
+                    y = -1;
+                    if ((k - j) % 16 == 0)
+                        System.out.print("\\");
+                    else if ((k - (12 - j)) % 16 == 0)
+                        System.out.print("/");
+                    else if (j == 3 && (k % 16 == 4 || k % 16 == 5 || k % 16 == 6 || k % 16 == 7 || k % 16 == 8))
+                        System.out.print("_");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+        for (int j = 3; j >= 1; j--) {
+            for (int k = 1; k <= 8 * n + 3; k++) {
+                if ((k - j) % 16 == 0 && k > 3)
+                    System.out.print("/");
+                else if ((k - (12 - j)) % 16 == 0 && k < 8 * n)
+                    System.out.print("\\");
+                else if (j == 1 && (k % 16 == 12 || k % 16 == 13 || k % 16 == 14 || k % 16 == 15 || k % 16 == 0))
+                    System.out.print("_");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    // private void showUpMap(int row) {
+    //     Tile[][] map = world.getMap();
+    //     int m = world.width;
+    //     int n = world.length;
+    //     int x = -1;
+    //     int y = -1;
+    //     boolean E = true;
+    //     boolean O = false;
+    //     boolean printingCordinatesFlag = false;
+    //     String cordinates = "";
+    //     int currentChar = 0;
+    //     for (int j = 3; j >= 1; j--) {
+    //         x = -1;
+    //         y = -1;
+    //         for (int k = 1; k <= 8 * n + 3; k++) {
+    //             if (j == 1 && k % 16 == 4) {
+    //                 printingCordinatesFlag = true;
+    //                 cordinates = x + "," + y;
+    //             }
+    //             if ((k - j) % 16 == 0 && (i > 1 || k < 8 * n)) {// on khar kaj akhary chap nashe
+    //                 System.out.print("/");
+    //                 y++;
+    //                 x++;
+    //             } else if ((k - (12 - j)) % 16 == 0) {
+    //                 System.out.print("\\");
+    //                 y--;
+    //                 x++;
+    //             } else if (j == 1 && (k % 16 == 12 || k % 16 == 13 || k % 16 == 14 || k % 16 == 15 || k % 16 == 0))
+    //                 System.out.print("_");
+    //             else {
+    //                 if (printingCordinatesFlag) {
+    //                     System.out.print(cordinates.charAt(currentChar));
+    //                     currentChar++;
+    //                     if (currentChar > (cordinates.length() - 1)) {
+    //                         currentChar = 0;
+    //                         printingCordinatesFlag = false;
+    //                     }
+    //                 } else
+    //                     System.out.print(" ");
+    //             }
+
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    // private void showDownMap() {
+
+    // }
 
     public void attack(Tile tile) {
 
