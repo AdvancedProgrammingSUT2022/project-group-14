1dbd68b11c22179ab3f9786feb01c1e4ebe6e97d
NikanV <nikanvsiuni@gmail.com>
2022-04-25 15:03:37 +0430

    world cleanup

--- src/main/java/models/World.java
+++ src/main/java/models/World.java
@@ -7,26 +7,48 @@ import java.util.HashMap;
 import enums.tiles.TileType;
 
 public class World {
-    private ArrayList<String> civilizationNames = new ArrayList<>();
-    private HashMap<String, Civilization> nations = new HashMap<>();
     private ArrayList<Civilization> civilizations = new ArrayList<>();
 
     private int year;
-
-    private static final int width = 45;
-    private static final int length = 80;
-    private Tile[][] map = new Tile[width][length];
+    private int evolutionSpeed;
     private int turn;
+
+    private static int width = 45;
+    private static int length = 80;
+    private Tile[][] map = new Tile[width][length];
+
 
 
     public World(ArrayList<String> players) {
-        civilizationNames.addAll(players);
-        for (String civilizationName : civilizationNames) {
-            nations.put(civilizationName, new Civilization(civilizationName));
+        for (int i = 0; i < players.size(); i++) {
+            civilizations.add(new Civilization(players.get(i)));
         }
-        this.year = -3000;
-        this.turn = 0;
+        year = -3000;
+        evolutionSpeed = 10;
+        turn = 0;
         generateMap();
+    }
+
+    public String getCurrentCivilizationName() {
+        return civilizations.get(turn).getName();
+    }
+
+    public Civilization getCivilizationByName(String name) {
+        for (Civilization civilization : civilizations) {
+            if (civilization.getName().equals(name))
+                return civilization;
+        }
+        return null;
+    }
+
+    public int getTurn() {
+        return this.turn;
+    }
+
+    public void nextTurn() {
+        turn++;
+        turn %= civilizations.size();
+        year += evolutionSpeed;
     }
 
     public static int getWidth() {
@@ -37,13 +59,6 @@ public class World {
         return length;
     }
 
-    public String getCurrentCivilizationName() {
-        return civilizationNames.get(turn);
-    }
-
-    public Civilization getCivilizationByName(String name) {
-        return nations.get(name);
-    }
 
     public Tile[][] getMap() {
         return this.map;
@@ -55,19 +70,6 @@ public class World {
                 this.map[i][j] = Tile.generateRandomTile(i, j);
             }
         }
-    }
-
-    public void nextTurn() {
-        this.turn++;
-        this.turn %= nations.size();
-    }
-
-    public int getTurn() {
-        return this.turn;
-    }
-
-    public HashMap<String, Civilization> getNations() {
-        return nations;
     }
 
     public Tile getTileByCoordinates(int x, int y) {
