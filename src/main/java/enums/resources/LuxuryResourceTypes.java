d7e342c92a5b691e86eca3b6d6898cdbd6f0b7a5
Ashakan Majidi <ashkan.majidi.1382@gmail.com>
2022-04-27 16:50:33 +0430

    LuxuryResources added

--- src/main/java/enums/resources/LuxuryResourceTypes.java
+++ src/main/java/enums/resources/LuxuryResourceTypes.java
@@ -1,5 +1,15 @@
 package enums.resources;
 
 public enum LuxuryResourceTypes {
-    
+    COTTON,
+    COLOR,
+    FUR,
+    JEWEL,
+    GOLD,
+    INCENSE,
+    IVORY,
+    MARBLE,
+    SILK,
+    SILVER,
+    SUGAR;
 }
