ab7d7105dc4884e893f1f0d979cec7884c7cc3a2
Ashakan Majidi <ashkan.majidi.1382@gmail.com>
2022-04-29 02:37:59 +0430

    StrategidResource json handlig deleted

--- src/main/java/models/resources/StrategicResource.java
+++ src/main/java/models/resources/StrategicResource.java
@@ -1,14 +1,7 @@
 package models.resources;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+=
 
 import enums.Improvements;
 import enums.Technologies;
@@ -23,47 +16,7 @@ public class StrategicResource extends Resource {
     private ArrayList<Building> dependentBuildings = new ArrayList<>();
     private ArrayList<Unit> dependentUnit = new ArrayList<>();
 
-    private static HashMap<StrategicResourceTypes, StrategicResource> typesMap = new HashMap<>();
 
-    public static void writeData() {
-        try {
-
-            FileWriter writer = new FileWriter("./src/main/resources/resources/StrategicResourceTypesInformation.json");
-            StrategicResource resource = null;
-            resource = new StrategicResource(StrategicResourceTypes.COAL, 0, 1, 0, Improvements.MINE,
-                    Technologies.SCIENTIFIC_THEORY);
-            typesMap.put(StrategicResourceTypes.COAL, resource);
-            resource = new StrategicResource(StrategicResourceTypes.HORSE, 0, 1, 0, Improvements.AGRICULTURE,
-                    Technologies.ANIMAL_HUSBANDRY);
-            typesMap.put(StrategicResourceTypes.HORSE, resource);
-            resource = new StrategicResource(StrategicResourceTypes.IRON, 0, 1, 0, Improvements.MINE,
-                    Technologies.IRON_WORKING);
-            typesMap.put(StrategicResourceTypes.IRON, resource);
-
-            writer.write(new Gson().toJson(typesMap));
-            writer.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public static void readData() {
-        try {
-            String json = new String(Files
-                    .readAllBytes(Paths.get("./src/main/resources/resources/StrategicResourceTypesInformation.json")));
-            typesMap = new Gson().fromJson(json, new TypeToken<HashMap<StrategicResourceTypes, StrategicResource>>() {
-            }.getType());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        for(var element: typesMap.entrySet())
-        {
-            System.out.println(element.getValue().getFood());
-        }
-    }
 
     public StrategicResource(StrategicResourceTypes type, double food, double production, double gold,
             Improvements requiredProgress, Technologies requiredTechnology) {
@@ -78,9 +31,6 @@ public class StrategicResource extends Resource {
         this.requiredTechnology = requiredTechnology;
     }
 
-    public static HashMap<StrategicResourceTypes, StrategicResource> typesMapGetter() {
-        return typesMap;
-    }
 
     public Building getBuilding() {
         return this.building;
