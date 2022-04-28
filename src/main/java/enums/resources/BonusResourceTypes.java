f06bb73674103004f553d97977daa1f368600e2c
Ashakan Majidi <ashkan.majidi.1382@gmail.com>
2022-04-29 02:35:18 +0430

    BonusResourceTypes enum fixed

--- src/main/java/enums/resources/BonusResourceTypes.java
+++ src/main/java/enums/resources/BonusResourceTypes.java
@@ -1,9 +1,24 @@
 package enums.resources;
 
-public enum BonusResourceTypes implements ResourceTypes{
-    BANANA,
-    COW,
-    GAZELLE,
-    SHEEP,
-    WHEAT;
+import enums.Improvements;
+
+public enum BonusResourceTypes implements ResourceTypes {
+
+    BANANA(1, 0, 0, Improvements.FARM),
+    COW(1, 0, 0, Improvements.CAMP),
+    GAZELLE(1, 0, 0, Improvements.CAMP),
+    SHEEP(2, 0, 0, Improvements.FARM),
+    WHEAT(1, 0, 0, Improvements.FARM);
+
+    private double food;
+    private double production;
+    private double gold;
+    private Improvements requiredImprovement;
+
+    BonusResourceTypes(double food, double production, double gold, Improvements requiredImprovement) {
+        this.food = food;
+        this.production = production;
+        this.gold = gold;
+        this.requiredImprovement = requiredImprovement;
+    }
 }
