c4bd0c2f3725a988a4fa6b34f2944065758dd036
Ashakan Majidi <ashkan.majidi.1382@gmail.com>
2022-05-04 23:56:01 +0430

    clean up and some features added

--- src/main/java/controllers/NewMapController.java
+++ src/main/java/controllers/NewMapController.java
@@ -2,20 +2,29 @@ package controllers;
 
 import enums.Colors;
 import models.Cell;
+import models.Tile;
 
 public class NewMapController {
     private static final int width = 3;
     private static final int length = 7;
 
     private static int[][][] tileCenter = new int[width][length][2];
-    private static Cell[][] map = new Cell[6 * width + 3][8 * length + 3];
+    private static Cell[][] outputMap = new Cell[6 * width + 3][8 * length + 3];
+    private static Tile[][] map = new Tile[width][length];
 
+    public static void generateMap() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < length; j++) {
+                map[i][j] = Tile.generateRandomTile(i, j);
+            }
+        }
+    }
+    
     private void upLayerBordersInit(int row) {
-
         for (int j = 0; j < 3; j++)
             for (int k = 0; k < 8 * length + 3; k++) {
                 Cell cell = new Cell();
-                map[6 * row + j][k] = cell;
+                outputMap[6 * row + j][k] = cell;
                 cell.setColor(Colors.RESET);
 
                 if (k % 16 == (2 - j) && (k < 8 * length || row > 0) && (k > 2 || row < width)) {
@@ -28,14 +37,13 @@ public class NewMapController {
                     cell.setCh(' ');
                 }
             }
-
     }
 
     private void downLayerBordersInit(int row) {
         for (int j = 0; j < 3; j++)
             for (int k = 0; k < 8 * length + 3; k++) {
                 Cell cell = new Cell();
-                map[6 * row + 3 + j][k] = cell;
+                outputMap[6 * row + 3 + j][k] = cell;
                 cell.setColor(Colors.RESET);
                 if (k % 16 == (0 + j)) {
                     cell.setCh('\\');
@@ -50,7 +58,6 @@ public class NewMapController {
     }
 
     private void bordersInit() {
-
         for (int i = 0; i <= width - 1; i++) {
             upLayerBordersInit(i);
             downLayerBordersInit(i);
@@ -67,7 +74,7 @@ public class NewMapController {
     public void showMap() {
         for (int i = 0; i < 6 * width + 3; i++) {
             for (int j = 0; j < 8 * length + 3; j++) {
-                System.out.print(map[i][j].getCh());
+                System.out.print(outputMap[i][j].getCh());
             }
             System.out.println();
         }
