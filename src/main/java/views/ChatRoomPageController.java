e3ed7597e16d720d1e19c20e7e463d2d25bf1466
NikanV <nikanvsiuni@gmail.com>
2022-06-05 16:50:52 +0430

    chatroom progress 2

--- src/main/java/views/ChatRoomPageController.java
+++ src/main/java/views/ChatRoomPageController.java
@@ -2,44 +2,91 @@ package views;
 
 import application.App;
 import controllers.UserController;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
 
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import models.Chat;
+import models.Message;
 import models.User;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Objects;
 
 public class ChatRoomPageController {
     @FXML
-    private VBox chatNames;
+    private Button privateChatButton;
     @FXML
-    private AnchorPane chatsPane;
+    private ScrollPane chatMessagesScrollPane;
     @FXML
-    private ChoiceBox<String> groupChatBox;
+    private TextField chatNameTextField;
     @FXML
-    private ChoiceBox<String> privateChatBox;
+    private TextField chatMessageTextField;
+    @FXML
+    private Text chatTitle;
+    @FXML
+    private VBox chatMessagesVBox;
+    @FXML
+    private VBox chatNamesVBox;
+    @FXML
+    private MenuButton usernamesMenuButton;
 
-    private Chat selectedChat;
+    private final ArrayList<Chat> selectedChats = new ArrayList<>();
 
 
     public void initialize() {
+        initPanes();
+        chatTitle.setText("");
+        chatMessagesVBox.heightProperty().addListener((observable, oldValue, newValue) -> {
+            if (chatMessagesVBox.getChildren().size() <= 9) {
+                chatMessagesScrollPane.setVvalue(0);
+            } else {
+                chatMessagesScrollPane.setVvalue(1.0d);
+            }
+        });
+        chatMessageTextField.setVisible(false);
+        chatMessageTextField.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode().getName().equals("Enter")) {
+                    Message message = new Message(chatMessageTextField.getText(), UserController.getLoggedInUser().getUsername(), new Date());
+                    for (Chat selectedChat : selectedChats) {
+                        selectedChat.addMessage(message);
+                    }
+                    HBox hBox = new HBox(new Circle(20, new ImagePattern(new Image(Objects.requireNonNull(UserController.getUserByUsername(message.getSenderUsername())).getAvatarFileAddress()))),
+                            new Text(" " + message.getSenderUsername() + " :\t" + message.getText() + "\t\t\t" + message.getDate().toString().substring(4, 16)));
+                    chatMessageTextField.setText("");
+                    hBox.setPrefWidth(chatMessagesVBox.getPrefWidth());
+                    hBox.setPrefHeight(50);
+                    chatMessagesVBox.getChildren().add(hBox);
+                }
+            }
+        });
+        chatNameTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                chatNameTextField.setPromptText("Enter your chat name");
+                chatNameTextField.setStyle("-fx-border-color: none");
+            }
+        });
+        addChatButtons();
+    }
+
+    public void initPanes() {
         for (User user : UserController.getUsers()) {
             if (user != UserController.getLoggedInUser()) {
-                privateChatBox.getItems().add(user.getUsername());
-                groupChatBox.getItems().add(user.getUsername());
+                usernamesMenuButton.getItems().add(new CheckMenuItem(user.getUsername()));
             }
-        }
-
-        for (Chat value : UserController.getLoggedInUser().getChats().values()) {
-            Button button = new Button(value.getName());
-            button.setStyle("-fx-border-radius: 0; -fx-background-color: #111a4d; -fx-pref-width: 350");
-            chatNames.getChildren().add(button);
         }
     }
 
@@ -47,12 +94,69 @@ public class ChatRoomPageController {
         App.changeScene("mainMenuPage");
     }
 
-    public void createPrivateChatClicked(MouseEvent mouseEvent) {
+    public void createChatClicked(MouseEvent mouseEvent) {
         ArrayList<String> usernames = new ArrayList<>();
-        usernames.add(privateChatBox.getValue());
-        usernames.add(UserController.getLoggedInUser().getUsername());
+        for (MenuItem item : usernamesMenuButton.getItems()) {
+            if (((CheckMenuItem) item).isSelected())
+                usernames.add(item.getText());
+        }
+        if (!creatingChatIsValid(usernames))
+            return;
+        if (mouseEvent.getSource().equals(privateChatButton) && usernames.size() > 2) {
+            chatNameTextField.setText("");
+            chatNameTextField.setPromptText("You can't add more than one person to private chats");
+            chatNameTextField.setStyle("-fx-border-color: #ff0022");
+            return;
+        }
+        for (int i = 0; i < usernames.size(); i++) {
+            Objects.requireNonNull(UserController.getUserByUsername(usernames.get(i))).addChats(new Chat(usernames, chatNameTextField.getText()));
+        }
+        addChatButtons();
+    }
+
+    public boolean creatingChatIsValid(ArrayList<String> usernames) {
+        if (chatNameTextField.getText().equals("")) {
+            chatNameTextField.setPromptText("You should give a name to your chat");
+            chatNameTextField.setStyle("-fx-border-color: #ff0022");
+            return false;
+        }
         for (String username : usernames) {
-            UserController.getUserByUsername(username).addChats(new Chat(usernames, "Chat -" + UserController.getUserByUsername(username).getChats().values().size()));
+            if (Objects.requireNonNull(UserController.getUserByUsername(username)).getChats().containsKey(chatNameTextField.getText())) {
+                chatNameTextField.setText("");
+                chatNameTextField.setPromptText("You or your friends have a chat with this name");
+                chatNameTextField.setStyle("-fx-border-color: #ff0022");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void addChatButtons() {
+        chatNamesVBox.getChildren().clear();
+        for (Chat value : UserController.getLoggedInUser().getChats().values()) {
+            Button button = new Button(value.getName());
+            button.setStyle("-fx-border-radius: 0; -fx-background-color: #111a4d; -fx-pref-width: 350");
+            button.setOnMouseClicked(mouseEvent -> {
+                for (String username : UserController.getLoggedInUser().getChats().get(button.getText()).getUsernames()) {
+                    selectedChats.add(Objects.requireNonNull(UserController.getUserByUsername(username)).getChats().get(button.getText()));
+                }
+                changeChat(UserController.getLoggedInUser().getChats().get(button.getText()));
+            });
+            chatNamesVBox.getChildren().add(button);
         }
     }
+
+    public void changeChat(Chat chat) {
+        chatTitle.setText(chat.getName());
+        chatMessageTextField.setVisible(true);
+        chatMessagesVBox.getChildren().clear();
+        for (Message message : chat.getMessages()) {
+            HBox hBox = new HBox(new Circle(20, new ImagePattern(new Image(Objects.requireNonNull(UserController.getUserByUsername(message.getSenderUsername())).getAvatarFileAddress()))),
+                    new Text(" " + message.getSenderUsername() + " :\t" + message.getText() + "\t\t\t" + message.getDate().toString().substring(4, 16)));
+            hBox.setPrefWidth(chatMessagesVBox.getPrefWidth());
+            hBox.setPrefHeight(50);
+            chatMessagesVBox.getChildren().add(hBox);
+        }
+    }
+
 }
