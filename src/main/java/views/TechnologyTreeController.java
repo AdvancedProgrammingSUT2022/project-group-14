0ae94d3102662fa547ab8a5dcf4b1898ef84c486
Ashkan <ashkan.majidi.1382@gmail.com>
2022-07-19 18:29:04 +0430

    tech tree added

---
+++ src/main/java/views/TechnologyTreeController.java
@@ -0,0 +1,13 @@
+package views;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class TechnologyTreeController {
+    @FXML
+    public AnchorPane mainPane;
+
+    public void initialize(){
+
+    }
+}
