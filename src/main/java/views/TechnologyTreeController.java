181da43d5e6f361144c323ff984bf8ff9a4ce1a0
Ashkan <ashkan.majidi.1382@gmail.com>
2022-07-19 18:32:31 +0430

    tech tree added

--- src/main/java/views/TechnologyTreeController.java
+++ src/main/java/views/TechnologyTreeController.java
@@ -1,13 +1,20 @@
 package views;
 
+import enums.Technologies;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+
+import java.util.HashSet;
 
 public class TechnologyTreeController {
     @FXML
     public AnchorPane mainPane;
 
-    public void initialize(){
+    public void initialize() {
+        initTechnologies();
+    }
 
+    public void initTechnologies() {
+        HashSet<Technologies> upLayers, currentLayer, DownLayer;
     }
 }
